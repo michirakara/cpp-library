c52b29d0eab158ee07beec84c5d4aaf3febc84fe
michirakara <naotokonagai@gmail.com>
2023-03-19 23:15:52 -0700

    a

--- verify/aoj/binary-indexed-tree.test.cpp
+++ verify/aoj/binary-indexed-tree.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_B"
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B"
 
 #include <bits/stdc++.h>
 using namespace std;
