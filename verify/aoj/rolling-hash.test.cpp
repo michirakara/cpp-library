aa4fcec082f93ba02e9fdb21e115408a1959c6d4
michirakara <naotokonagai@gmail.com>
2023-03-19 23:02:56 -0700

    バグってた

--- verify/aoj/rolling-hash.test.cpp
+++ verify/aoj/rolling-hash.test.cpp
@@ -9,7 +9,9 @@ int main(){
     string T,P;cin>>T>>P;
     int base=27+rand()%50;
     RollingHash t(T,base),p(P,base);
-
+    if(T.size()<P.size()){
+        exit(0);
+    }
     for(int i=0;i<=T.size()-P.size();i++){
         if(t.hash(i,i+P.size())==p.hash(0,P.size()))cout<<i<<endl;
     }
