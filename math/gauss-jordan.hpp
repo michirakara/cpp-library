75a99fcae8adfea2ff8d1ef876d0af650cbfd2b2
michirakara <naotokonagai@gmail.com>
2023-09-29 10:21:17 -0700

    行列で連立方程式を解くライブラリを書きました

---
+++ math/gauss-jordan.hpp
@@ -0,0 +1,38 @@
+#include<vector>
+#include<cassert>
+#include<utility>
+#include<math/matrix.hpp>
+template<class T>
+struct GaussJordan{
+    Matrix<T> mat;
+
+    GaussJordan(Matrix<T> system):mat(system){
+        assert(mat.height()==mat.width()-1);
+    }
+    std::vector<T> solve(){
+        for(int i=0;i<mat.width()-1;i++){
+            for(int j=i;j<mat.height();j++){
+                if(mat[j][i]!=0){
+                    std::swap(mat[j],mat[i]);
+                    T to_div=mat[i][i];
+                    for(int k=0;k<mat.width();k++){
+                        mat[i][k]/=to_div;
+                    }
+                    for(int k=0;k<mat.height();k++){
+                        if(i==k)continue;
+                        T mul_by=-mat[k][i];
+                        for(int l=0;l<mat.width();l++){
+                            mat[k][l]+=mul_by*mat[i][l];
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        std::vector<T> ret(mat.height());
+        for(int i=0;i<mat.height();i++){
+            ret[i]=mat[i][mat.width()-1];
+        }
+        return ret;
+    }
+};
